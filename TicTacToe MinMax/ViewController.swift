d99fdf64bba0fc721c18d0c881bda24db2367358
José Roberto Abreu Báez <joserobertoabreu1@hotmail.com>
2016-07-25 10:03:49 -0400

    Initial Commit

---
+++ TicTacToe MinMax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToe MinMax
+//
+//  Created by Roberto Abreu on 25/7/16.
+//  Copyright © 2016 homeappz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
